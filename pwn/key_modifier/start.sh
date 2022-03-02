8ef5549e4200b932daf7b097fedbc3740bce7413
Ooggle <33269056+Ooggle@users.noreply.github.com>
2022-03-02 14:15:28 +0100

    change pwn medium port

--- pwn/key_modifier/start.sh
+++ pwn/key_modifier/start.sh
@@ -2,5 +2,5 @@
 
 while :
 do
-	exec socat TCP-LISTEN:55556,reuseaddr,fork,forever,keepalive EXEC:'/home/ctf/challenge'
+	exec socat TCP-LISTEN:55555,reuseaddr,fork,forever,keepalive EXEC:'/home/ctf/challenge'
 done
